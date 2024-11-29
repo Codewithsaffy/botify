cb4dace21d1c918071958b87afefd5040b503b7f
codewithsaffy <codewithsaffy@gmail.com>
2024-11-29 09:44:22 +0500

    correct profile

--- src/app/api/profile/[username]/route.ts
+++ src/app/api/profile/[username]/route.ts
@@ -62,9 +62,9 @@ export async function GET(
       },
     ]);
     if (profileData) {
-      return NextResponse.json(profileData, { status: 200 });
+      return NextResponse.json({profileData}, { status: 200 });
     }
-    return NextResponse.json({ message: "User not found" }, { status: 404 });
+    return NextResponse.json({ message: "User not found" }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { message: "Internal Server Error" },
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,6 @@
 export default function Loading() {
   return <div className="flex justify-center min-h-screen items-center">
     <div className="loader"></div>
- 
   </div>;
 
 }
